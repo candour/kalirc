c811eee8a52eacb0ac5fd98a7ed99f597bd1f5ae
candour <4670475+candour@users.noreply.github.com>
2025-11-23 22:18:01 +1100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("androidx.room") version "$room_version" apply false
+    id("androidx.room") version "2.8.3" apply false
 }
 
 allprojects {
